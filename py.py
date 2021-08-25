0b95f207f29928979212a76f65b832d2788fa6f7
manishuniyal <manishuniyal70@gmail.com>
2021-08-26 00:01:05 +0530

    new

--- py.py
+++ py.py
@@ -1,54 +1,83 @@
+from pptx import Presentation
+from wand import assertions
 from wand.image import Image
 #from wand.color import Color
 #from wand.drawing import Drawing
 from wand.display import display
-from pptx import Presentation
 import os
-imgarr=[
+from pptx.util import Inches
+
+prs = Presentation()
+test_image=[
   "/home/manish/Downloads/cc1.jpg",
   "/home/manish/Downloads/cc3.jpg",
   "/home/manish/Downloads/cc4.jpg",
   "/home/manish/Downloads/cc5.jpg",
   "/home/manish/Downloads/cc6.jpg"]
 
+
 output_img=[
   "img1.jpeg", 
   "img2.jpeg",
-   "img3.jpeg", 
-   "img4.jpeg",
-   "img5.jpeg" ]
+  "img3.jpeg", 
+  "img4.jpeg",
+  "img5.jpeg" ]
 
-output_img=[
-  "img1.pptx", 
-  "img2.pptx",
-   "img3.pptx", 
-   "img4.pptx",
-   "img5.pptx" ]
-
-'''for i in range(len(imgarr)):
-  with Image(filename =imgarr[i]) as image:
+b=["jpeg", "jpg"]
+for i in range(len(test_image)):
+  assert test_image[i][-3:] in b, "oops image not in supported format"
+  with Image(filename =test_image[i]) as image:
       # Import the watermark image
       with Image(filename ='/home/manish/Downloads/cc2.png' ) as water_mark:
           # Clone the image in order to process
           water_mark.resize(1300,500)
           with image.clone() as watermark:
-              # Invoke watermark function with watermark image, transparency as 0.5
-              # left as 10 and top as 20
+              # Invoke watermark function with [watermark image, transparency as 0
+              # left as 10 and top as 180]
               watermark.watermark(water_mark, 0, 10, 180)
                   # Save the image
               display(watermark)
-              watermark.save(filename =output_img[i])'''
-a=[]
-for i in range(len(imgarr)):
-  prs = Presentation()
-  slide = prs.slides.add_slide(prs.slide_layouts[8])
+              watermark.save(filename =output_img[i])
 
-  title=slide.shapes.title # assigning a title
-  placeholder=slide.placeholders[1] # placeholder for subtitle
-  title.text="Hey,This is a Slide! How exciting!" # title
-  placeholder.text="Really?" # sub title
-
-  picture = placeholder.insert_picture(imgarr[i])
-  prs.save(output_img[i])
-  
-  #os.startfile("ESEMPIO.pptx")
+class MySlide:
+    def __init__(self, data):
+        self.layout = prs.slide_layouts[data[3]]
+        self.slide=prs.slides.add_slide(self.layout)
+        self.title=self.slide.shapes.title
+        self.title.text=data[0]
+        self.subtitle=self.slide.placeholders[1]
+        self.subtitle.text=data[1]
+        top = Inches(2.3) 
+        left = Inches(0.1) 
+        height = Inches(5) 
+        pic = self.slide.shapes.add_picture(data[2],left,top, height=height) 
+        #self.placeholders[2].insert_picture(data[2])
+ 
+slides = [
+    ["It's me Title1",       #data[0]
+     "It's me sub Title1)",
+     output_img[0],
+     3],
+    ["It's me Title2",       #data[0]
+     "It's me sub Title2)",
+     output_img[1],
+     3],
+    ["It's me Title3",       #data[0]
+     "It's me sub Title3",
+     output_img[2],
+     3],
+    ["It's me Title4",       #data[0]
+     "It's me sub Title4",
+     output_img[3],
+     3],
+    ["It's me Title5",       #data[0]
+     "It's me sub Title5",
+     output_img[4],
+     3]
+]
+ 
+for each_slide in slides:
+    MySlide(each_slide)
+ 
+prs.save("Final_output.pptx")
+#os.startfile("stack.pptx")  "it is run only windows os"
--- python.py
+++ python.py
@@ -1,8 +1,6 @@
 from pptx import Presentation
 from wand import assertions
 from wand.image import Image
-#from wand.color import Color
-#from wand.drawing import Drawing
 from wand.display import display
 import os
 from pptx.util import Inches
@@ -25,7 +23,7 @@ output_img=[
 
 b=["jpeg", "jpg"]
 for i in range(len(test_image)):
-  assert test_image[i][-1,-4] in b
+  assert test_image[i][-3:] in b, "oops image not in supported format"
   with Image(filename =test_image[i]) as image:
       # Import the watermark image
       with Image(filename ='/home/manish/Downloads/cc2.png' ) as water_mark:
@@ -38,46 +36,44 @@ for i in range(len(test_image)):
                   # Save the image
               display(watermark)
               watermark.save(filename =output_img[i])
+def fun1(data):
+  layout = prs.slide_layouts[data[3]]
+  slide=prs.slides.add_slide(layout)
+  title=slide.shapes.title
+  title.text=data[0]
+  subtitle=slide.placeholders[1]
+  subtitle.text=data[1]
+  top = Inches(2.3) 
+  left = Inches(0.1) 
+  height = Inches(5) 
+  slide.shapes.add_picture(data[2],left,top, height=height) 
+     
 
-class MySlide:
-    def __init__(self, data):
-        self.layout = prs.slide_layouts[data[3]]
-        self.slide=prs.slides.add_slide(self.layout)
-        self.title=self.slide.shapes.title
-        self.title.text=data[0]
-        self.subtitle=self.slide.placeholders[1]
-        self.subtitle.text=data[1]
-        top = Inches(2.3) 
-        left = Inches(0.1) 
-        height = Inches(5) 
-        pic = self.slide.shapes.add_picture(data[2],left,top, height=height) 
-        #self.placeholders[2].insert_picture(data[2])
- 
 slides = [
     ["It's me Title1",       #data[0]
      "It's me sub Title1)",
      output_img[0],
      3],
-    ["It's me Title2",       #data[0]
+    ["It's me Title2",       #data[1]
      "It's me sub Title2)",
      output_img[1],
      3],
-    ["It's me Title3",       #data[0]
+    ["It's me Title3",       #data[2]
      "It's me sub Title3",
      output_img[2],
      3],
-    ["It's me Title4",       #data[0]
+    ["It's me Title4",       #data[3]
      "It's me sub Title4",
      output_img[3],
      3],
-    ["It's me Title5",       #data[0]
+    ["It's me Title5",       #data[4]
      "It's me sub Title5",
      output_img[4],
      3]
-]
+    ]
  
 for each_slide in slides:
-    MySlide(each_slide)
+    fun1(each_slide)
  
-prs.save("Final_output.pptx")
-#os.startfile("stack.pptx")  "it is run only windows os"
+prs.save("output.pptx")
+#os.startfile("stack.pptx")  "this command run only windows os"
